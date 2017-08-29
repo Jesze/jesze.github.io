abb3a9d4411a44c0a5ccc37c2729ae5d3d72c136
Jesze-Surface <jesze@forestbear.net>
2017-08-29 14:08:21 -0600

    Added image wireframe mask.

---
+++ js/mask.js
@@ -0,0 +1,12 @@
+
+$('.model').mousemove(function(e){
+  
+  var offs = $(this).offset(),
+      p    = {x:offs.left, y:offs.top},
+      mPos = {x:e.pageX, y:e.pageY},
+      x    = mPos.x - p.x - 100,
+      y    = mPos.y - p.y - 100;
+      
+  $('.gray', this).css({left:x, top:y, backgroundPosition: -x+'px '+-y+'px'});
+    
+});
