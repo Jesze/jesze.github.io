8aec6d54ff80ba05b97e698879bf0fd09acbe7b6
Jesze-Surface <jesze@forestbear.net>
2017-08-29 14:58:15 -0600

    Added check for if mouse is still hovering over image.

--- js/mask.js
+++ js/mask.js
@@ -1,12 +1,18 @@
 
-$('.model').mousemove(function(e){
-  
-  var offs = $(this).offset(),
-      p    = {x:offs.left, y:offs.top},
-      mPos = {x:e.pageX, y:e.pageY},
-      x    = mPos.x - p.x - 100,
-      y    = mPos.y - p.y - 100;
-      
-  $('.gray', this).css({left:x, top:y, backgroundPosition: -x+'px '+-y+'px'});
+
+$('.model')
+.mousemove(function(e)
+{
+	var offs = $(this).offset(),
+		p    = {x:offs.left, y:offs.top},
+		mPos = {x:e.pageX, y:e.pageY},
+		x    = mPos.x - p.x - 100,
+		y    = mPos.y - p.y - 100;
+
+	$('.gray', this).css({left:x, top:y, backgroundPosition: -x+'px '+-y+'px', width:200, height:200});
     
+})
+.mouseleave(function(m)
+{
+	$('.gray', this).css({width:0, height:0});	
 });
