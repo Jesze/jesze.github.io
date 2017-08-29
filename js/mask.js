5b7cb2bbc8c3bbbf865c58e122229459837509e0
ally <cikor.allyson@gmail.com>
2017-08-29 15:41:20 -0600

    Added timed shrink to leave event.

--- js/mask.js
+++ js/mask.js
@@ -1,4 +1,6 @@
-
+var xsize = 100, ysize = 100;
+var leavex, leavey;
+var shrink;
 
 $('.model')
 .mousemove(function(e)
@@ -6,13 +8,35 @@ $('.model')
 	var offs = $(this).offset(),
 		p    = {x:offs.left, y:offs.top},
 		mPos = {x:e.pageX, y:e.pageY},
-		x    = mPos.x - p.x - 100,
-		y    = mPos.y - p.y - 100;
+		x    = mPos.x - p.x - xsize,
+		y    = mPos.y - p.y - ysize;
 
-	$('.gray', this).css({left:x, top:y, backgroundPosition: -x+'px '+-y+'px', width:200, height:200});
+	$('.gray', this).css({left:x, top:y, backgroundPosition: -x+'px '+-y+'px', width:xsize*2, height:ysize*2});
     
 })
-.mouseleave(function(m)
+.mouseleave(function(p)
+{	
+	leavex = p.pageX;
+	leavey = p.pageY;
+	shrink = setInterval(shrinkCircle,5);
+})
+
+.mouseenter(function(p)
 {
-	$('.gray', this).css({width:0, height:0});	
+	clearInterval(shrink);
+	xsize = 100;
+	ysize = 100;
 });
+
+function shrinkCircle()
+{
+	xsize--;
+	ysize--;
+	var offs = $('.model').offset(),
+		p    = {x:offs.left, y:offs.top},
+		mPos = {x:leavex, y:leavey},
+		x    = mPos.x - p.x - xsize,
+		y    = mPos.y - p.y - ysize;
+
+	$('.gray', '.model').css({left:x, top:y, backgroundPosition: -x+'px '+-y+'px', width:xsize*2, height:ysize*2});	
+}
